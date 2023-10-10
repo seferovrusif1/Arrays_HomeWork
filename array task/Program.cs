fd48052d77d695d39c11c1f7d1b8dcd3087ae3de
seferovrusif1 <mi73sbd9a@code.edu.az>
2023-10-10 21:27:22 +0400

    added array task correctly

---
+++ array task/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace array_task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 6, 41, 9, 12, 5, 23 };
+            int[] newnums = new int[nums.Length];      //evvelki array uzunluqunda yeni array yaradiriq
+
+            for (int i = 1; i < nums.Length+1; i++)
+            {
+                newnums[nums.Length-i] = nums[i-1];    //yeni arraya elementleri tersden mensub edirik
+            }
+            for (int i = 0; i <  newnums.Length; i ++)
+            {
+                Console.WriteLine(newnums[i]);         //Yeni arrayi capa veririk
+
+            }
+        }
+    }
+}
